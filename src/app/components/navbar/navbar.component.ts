d39ec5fc8a178d9b4e38f6e6196ea764dcfd9a2f
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-02-11 14:56:43 +0000

    Fix Icon Toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,21 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  public toggleIcon = ['fas', 'bars'];
   public isMenuCollapsed = true;
   constructor() { }
 
   ngOnInit(): void {
-    
-    console.log(this.toggleIcon);
-
-  }
-
-  toggleIconAndIsMenuCollapsed(){
-    this.isMenuCollapsed = !this.isMenuCollapsed
-    this.toggleIcon = this.isMenuCollapsed ?  ['fas', 'bars'] : ['far', 'times-circle'];
-  }
-
-  
+  } 
 
 }
