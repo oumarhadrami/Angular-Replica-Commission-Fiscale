ba6dd9830f69d8fa696c1b6d8ba7e6d6cd43526a
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-02-15 13:50:47 +0000

    Fetch Arabic News based on language

--- src/app/components/older-news/older-news.component.ts
+++ src/app/components/older-news/older-news.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { News } from 'src/app/models/news';
+import { LanguageService } from 'src/app/services/language.service';
 import { NewsService } from 'src/app/services/news.service';
 
 @Component({
@@ -11,8 +12,13 @@ export class OlderNewsComponent implements OnInit {
   olderNewsList: News[] = [];
   page = 0;
   size = 8;
+  currentLang?: string;
 
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService,private languageService: LanguageService) {
+    this.languageService.currentLang.subscribe((currentLang) => {
+      this.currentLang = currentLang;
+   })
+  }
 
   ngOnInit(): void {
     this.retrieveOlderNews();
